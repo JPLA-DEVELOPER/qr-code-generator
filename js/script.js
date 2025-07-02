e6303cbb3a8a75fd9f0b7129c72a14bf3f4d30d8
João Paulo  Lima de Albuquerque <jpla.developer@gmail.com>
2025-07-02 09:38:36 -0300

    Finalização do projeto.

--- js/script.js
+++ js/script.js
@@ -32,27 +32,64 @@ function generateQrCode(){
    
 }  
 
-function downloadQrCode (){
-    const qrCodeInputValue = qrCodeInput.value; 
-    if(!qrCodeInputValue) return; 
-    qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
-    
-    
-}
+function generateQrCode2 () {
+    // Pega o valor do input
+    const qrCodeInputValue = qrCodeInput.value;  
+    console.log(`Valor: ${qrCodeInputValue}`);
+  
+    if (!qrCodeInputValue) return;
+  
+    // Modifica o texto do botão
+    qrCodeBtn.innerText = "Gerando código...";
+  
+    const qrCodeContainer = document.querySelector(".qr-code-download");
+  
+    // Limpa QR Code anterior
+    qrCodeContainer.innerHTML = "";
+  
+    // Cria novo QR Code
+    const qrcode = new QRCode(qrCodeContainer, {
+      text: qrCodeInputValue,
+      width: 128,
+      height: 128
+    });
+  
+    // Marca o container como ativo
+    container.classList.add("active");
+  
+    // Aguarda um pequeno tempo para considerar o QR gerado
+    setTimeout(() => {
+      qrCodeBtn.innerText = "QR Code criado com sucesso!";
+    }, 300);
+  }
+
+  //Função para baixar o QR Code
+function downloadQrCode() {
+    const canvas = document.querySelector(".qr-code-download canvas");
+  
+  
+    const imageData = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = imageData;
+    link.download = "qrcode.png";
+    link.click();
+  }
+  
 
 //Eventos
-//Evento de clique no botão
+//Evento de clique no botão gerar
 qrCodeBtn.addEventListener("click", () => {
-    generateQrCode();
+    generateQrCode2();
 });
 
-//Evento da tecla Enter
+//Evento da tecla Enter para gerar
 qrCodeInput.addEventListener("keydown", (e) => {
     if(e.code === "Enter"){
-        generateQrCode();
+        generateQrCode2();
     }
 })
 
+//Evento do botão de dowload
 qrCodeBtnDownload.addEventListener("click", () => {
     console.log("Download solicitado");
     downloadQrCode();
